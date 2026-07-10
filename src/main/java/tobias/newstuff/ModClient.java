1348d7fd1cd6aa4ad726c56d3da7d2b40d4fb33d
Tobias <tobias.kruse@stud.uni-hannover.de>
2026-07-11 00:57:33 +0200

    Initial commit

---
+++ src/main/java/tobias/newstuff/ModClient.java
@@ -0,0 +1,10 @@
+package tobias.newstuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
